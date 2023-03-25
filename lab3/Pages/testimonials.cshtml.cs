62d385d2925b8c967941e96dc10d46a9d0a9bc2d
milvish <78490244+milvish@users.noreply.github.com>
2023-03-25 22:59:51 +0300

    Add project files.

---
+++ lab3/Pages/testimonials.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace lab3.Pages
+{
+    public class testimonialsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
